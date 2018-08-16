ed34b33c6633fdf33dcaef4aa594b1aa00a0ac48
dezert99 <bubbamart99@gmail.com>
2018-08-16 17:18:33 -0400

    Added plant growth system and removed inventory
    Plants are now sold on harvest

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 /*TODO
 Simplify plant function to just one function. 
 */
-function guidGenerator() {
-    var S4 = function() {
-       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
-    };
-    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
-}
+let player, currentCommand, water = 5;
+let timeToGrow = new Map();
+timeToGrow.set("tomato",6);
+timeToGrow.set("potato",8);
+
+let sellPrice = new Map();
+sellPrice.set("tomato",10);
+sellPrice.set("potato",15);
+
 class Player {
     constructor(tomato,potato,gold){
         this.tomato=tomato;
@@ -21,45 +24,62 @@ class Player {
 class Plant {
     constructor(name, plotPos){
         this.name=name;
+        this.age = 0;
         this.harvestable = false;
         this.plotPos = plotPos; // Plant's position in plot array
         this.watered = false;
         this.penalty = 0; // If the plant is not watered after a day, there will be a penalty in its selling price
     }
     harvest(){
-        console.log("Harvesting "+ this.name);
+        console.log("Harvesting "+ this.name + " with penalty " + this.penalty);
         player.plot[player.plot.indexOf(this)] = null; // Remove plant from plot
-        player.inventory.set(this.name, player.inventory.get(this.name)+1); // Increases tomatos in players inventory
-        document.querySelector("#plot-"+this.plotPos).innerHTML = "Plant here!"; // Resets button text. 
-        //This whole if else statment is just to update the players inventory on screen. 
-        if(document.querySelector("#"+this.name+"SellNum")==null){
-            let inventory = document.querySelector(".inventory");
-            let plantDiv = document.createElement("div");
-            plantDiv.appendChild(document.createTextNode(this.name.charAt(0).toUpperCase() + this.name.slice(1)+": "));
-            let plantCount = document.createElement("p");
-            plantCount.innerHTML = 1;
-            plantCount.setAttribute("id",this.name+"AmtNum")
-            plantDiv.appendChild(plantCount);
-            let tCount = document.createElement("input");
-            tCount.setAttribute("id",this.name+"SellNum");
-            tCount.setAttribute("type","number");
-            tCount.setAttribute("min", 1);
-            tCount.setAttribute("max", 1);
-            tCount.setAttribute("step", 1);
-            tCount.value = 0;
-            plantDiv.appendChild(tCount);
-            inventory.appendChild(plantDiv);
-        }
-        else{
-            document.querySelector("#"+this.name+"AmtNum").innerHTML = player.inventory.get(this.name);
-            document.querySelector("#"+this.name+"SellNum").setAttribute("max",player.inventory.get(this.name));
-        }
+        player.gold += sellPrice.get(this.name) - this.penalty;
+        // player.inventory.set(this.name, player.inventory.get(this.name)+1); // Increases tomatos in players inventory
+        // document.querySelector("#plot-"+this.plotPos).innerHTML = "Plant here!"; // Resets button text. 
+        // //This whole if else statment is just to update the players inventory on screen. 
+        // if(document.querySelector("#"+this.name+"SellNum")==null){
+        //     let inventory = document.querySelector(".inventory");
+        //     let plantDiv = document.createElement("div");
+        //     plantDiv.appendChild(document.createTextNode(this.name.charAt(0).toUpperCase() + this.name.slice(1)+": "));
+        //     let plantCount = document.createElement("p");
+        //     plantCount.innerHTML = 1;
+        //     plantCount.setAttribute("id",this.name+"AmtNum")
+        //     plantDiv.appendChild(plantCount);
+        //     let tCount = document.createElement("input");
+        //     tCount.setAttribute("id",this.name+"SellNum");
+        //     tCount.setAttribute("type","number");
+        //     tCount.setAttribute("min", 1);
+        //     tCount.setAttribute("max", 1);
+        //     tCount.setAttribute("step", 1);
+        //     tCount.value = 0;
+        //     plantDiv.appendChild(tCount);
+        //     inventory.appendChild(plantDiv);
+        // }
+        // else{
+        //     document.querySelector("#"+this.name+"AmtNum").innerHTML = player.inventory.get(this.name);
+        //     document.querySelector("#"+this.name+"SellNum").setAttribute("max",player.inventory.get(this.name));
+        // }
     }
     grow(){
-        this.harvestable = true;
+        this.age++;
+        console.log("I'm a " + this.name + " and I'm " + this.age + " days old.")
+        if(this.age == timeToGrow.get(this.name)){ // Checking to see if it grows again, will it be harvestable
+            this.harvestable = true;
+            let btn = document.querySelector("#plot-"+this.plotPos);
+            btn.innerHTML = this.name.charAt(0).toUpperCase() + this.name.slice(1) + ": Harvest me!";
+        } // maybe add in mechanic that if they keave a harvestable plant in it will start to go bad.
+        else{
+            if(this.watered == false){
+                this.penalty = this.penalty * 2 + 1;
+            }
+            else{
+                this.watered = false;
+            }
+        }
     }
+
 }
-let player, currentCommand, water = 5;
+
 
 function buySeed(seedName,price){
     price = parseInt(price);
@@ -83,6 +103,7 @@ function interact(plotNum){
             console.log("Nothing to water!")
         }
         else{
+            console.log("Watering plot "+plotNum);
             player.plot[parseInt(plotNum)].watered = true;
         }
 
@@ -106,7 +127,6 @@ function interact(plotNum){
             console.log("Plant already planted");
         }
         // player.plot.push(plant);  //push plant onto plot
-        window.setTimeout(plant.grow,10000);//grow plant after 10 seconds
         eval("player."+currentCommand+" --");
         
     }
@@ -124,6 +144,14 @@ function sell(plantName){
     else{
         console.log("No "+plantName+"'s to sell!")
     }
+}
+
+function advanceDay(){
+    player.plot.forEach(function(plant) {
+        if(plant != null){
+            plant.grow();
+        }
+    })
 }
 
 window.onload = function WindowLoad(event) {
